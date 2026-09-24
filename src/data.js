0cdc07410d25fb8ace57b1205bf236addda98530
Nikhil-Chaudhary121 <Nikhilchaudhary9887@gmail.com>
2026-09-24 18:08:42 +0530

    name change

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@
 const img = (seed, w = 800, h = 800) => `https://picsum.photos/seed/${seed}/${w}/${h}`;
 
 export const brand = {
-  name: 'Ram Gaming World',
+  name: 'JD Game Zone',
   tagline: 'Everyday goods, chosen well',
 };
 
